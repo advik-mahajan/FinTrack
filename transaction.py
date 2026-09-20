59b3a61e723a97783d6c4b842250ffd0a387efd2
Advik M <advik.mahajan121802@gmail.com>
2026-09-21 03:45:46 +0530

    Implement Transaction class with details display
    Added a Transaction class to manage transaction details.

--- transaction.py
+++ transaction.py
@@ -1,1 +1,22 @@
+import datetime
 
+class Transaction:
+    transaction_id_counter = 0
+    def __init__(self, user_id, transaction_type, category, amount, description):
+        self.user_id = user_id
+        self.transaction_type = transaction_type
+        self.category = category
+        self.amount = amount
+        self.date = datetime.date.today().strftime("%d/%m/%Y")
+        self.description = description
+        Transaction.transaction_id_counter += 1
+        self.transaction_id = Transaction.transaction_id_counter
+
+    def display_transaction_details(self):
+        print("===TRANSACTION DETAILS===")
+        print(f"TRANSACTION ID (auto-generated) : {self.transaction_id}")
+        print(f"TRANSACTION TYPE: {self.transaction_type}")
+        print(f"CATEGORY: {self.category}")
+        print(f"AMOUNT:  ₹{self.amount}")
+        print(f"DESCRIPTION: {self.description}")
+        print(f"DATE: {self.date}")
